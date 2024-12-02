16ca4e26edf6bb6e98b034a96dcc10efe2ee1f01
Sujandas241 <sujan.das014241@rec.ac.in>
2024-12-02 10:33:11 +0530

    Add files via upload

---
+++ Arithmetic.js
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>This is my web page</title>
+</head>
+<body>
+    <script>
+        let number1=parseFloat(prompt("Enter the 1st number:"));
+        let number2=parseFloat(prompt("Enter the 2nd number:"));
+
+        let sum=number1+number2;
+        let difference=number1-number2;
+        let let product=number1*number2;
+        
+        document.writtenln("Sum" + sum);
+        document.writtenln("Difference" + difference);
+        document.writtenln("Product" + product);
+
+    </script> document.writtenln("Sum" + sum);
+
+</body>
+</html>
